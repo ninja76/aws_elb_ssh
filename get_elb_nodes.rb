60b3944880ad4f6d09b708454a4f51ba4b740214
root <bryan.haase@dice.com>
2014-03-27 20:47:56 +0000

    Added optional prefix to allow for easier naming of cluster members

--- get_elb_nodes.rb
+++ get_elb_nodes.rb
@@ -8,6 +8,7 @@ begin
     elb_group = opts[:name]
     elb = AWS::ELB.new
     ec2 = AWS::EC2.new
+    c = 0
     instance_ids = elb.load_balancers[elb_group].instances.collect(&:id)
     instance_ids.each do |id|
       i = ec2.instances[id]
@@ -17,9 +18,15 @@ begin
         puts "#{i.id} #{name} #{i.private_ip_address}"
       end
       if opts[:ssh]
+	c = c +1
 	ssh_user = opts[:user]
 	ssh_key  = opts[:key]
-	puts "Host #{name}\n"
+        prefix   = opts[:prefix]
+	if prefix != ""
+	  puts "Host #{prefix}#{c}\n"
+	else
+	  puts "Host #{name}\n"
+        end
    	puts "  Hostname #{i.private_ip_address}\n"
         puts "  IdentityFile \"#{ssh_key}\"\n"
 	puts "  User #{ssh_user}\n\n"
@@ -34,6 +41,7 @@ begin
       opt :ssh, "Output SSH config"
       opt :user, "SSH User", :type => :string
       opt :key, "SSH Key File", :type => :string
+      opt :prefix, "Prefix for each instance ex. web results in web1, web2, etc", :type => :string
     end
     return opts
   end
