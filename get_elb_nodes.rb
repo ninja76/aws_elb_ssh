1e994dd0b0193564cbdc705a3aba75f69b61bc59
root <bryan.haase@dice.com>
2014-04-15 15:11:18 +0000

    Added config file to store ELB information

--- get_elb_nodes.rb
+++ get_elb_nodes.rb
@@ -1,10 +1,34 @@
 #!/usr/bin/ruby
 require 'aws-sdk'
 require 'trollop'
-require File.expand_path(File.dirname(__FILE__) + '/config/config.rb')
+require 'json'
+require File.expand_path(File.dirname(__FILE__) + '/config/config.aws.rb')
 
 begin
-  def fetch_instance_ips(opts)
+  def fetch_instances_w_config(config_options)
+    elb = AWS::ELB.new
+    ec2 = AWS::EC2.new
+    JSON.parse(File.read("predefined.json"))['groups'].each do |item|
+      elb_group = item['elbName']
+      ssh_user = item['username']
+      ssh_key  = item['sshKey']
+      prefix   = item['groupName']
+      c = 0
+      instance_ids = elb.load_balancers[elb_group].instances.collect(&:id)
+      instance_ids.each do |id|
+        i = ec2.instances[id]
+        tags = i.tags
+        name = tags[:Name]
+        c = c +1
+        puts "Host #{prefix}#{c}\n"
+        puts "  Hostname #{i.private_ip_address}\n"
+        puts "  IdentityFile \"#{ssh_key}\"\n"
+        puts "  User #{ssh_user}\n\n"
+      end
+    end
+  end
+
+  def fetch_instances_wo_config(opts)
     elb_group = opts[:name]
     elb = AWS::ELB.new
     ec2 = AWS::EC2.new
@@ -37,6 +61,7 @@ begin
 ## Get CLI options
   def parse_options
     opts = Trollop::options do
+      opt :config, "Config File", :type => :string
       opt :name, "ELB Name", :type => :string
       opt :ssh, "Output SSH config"
       opt :user, "SSH User", :type => :string
@@ -48,4 +73,9 @@ begin
 end
 
 opts = parse_options
-fetch_instance_ips opts 
+if opts[:config]
+  config_options = JSON.parse(File.read("predefined.json"));
+  fetch_instances_w_config config_options 	
+else
+  fetch_instances_wo_config opts 
+end
